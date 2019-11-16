7447b24553fc90dbfcc3a75c80fd7ec3a5a03046
Jacob Kocina <Jkocina.jr@gmail.com>
2019-11-16 12:50:56 -0600

    formatting email

--- routes/contact.js
+++ routes/contact.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 
 
 //This is information for the email
-var emailInfo;
+/*var emailInfo;
 fs.readFile('data/email.html', 'utf8', function(err, data){
     if (err) {
       throw err;
@@ -14,23 +14,22 @@ fs.readFile('data/email.html', 'utf8', function(err, data){
       emailInfo = data;
     }
   });
+*/
 
-var config;
-
-//Testing for the heroku environment variables
 if (!process.env.SENDGRID_API_KEY) {
   //reading the sendGrid json file if we are in production
   fs.readFile('data/sendgrid.json', 'utf8', function(err, data){
       if (err) {
         throw err;
       } else {
-
         config = JSON.parse(data);
+        key = config[0].SENDGRID_API_KEY;
       }
-  });
+  })
+
+} else {
+  key = process.env.SENDGRID_API_KEY;
 }
-
-
 
 /* GET contacts home page. */
 router.get('/', function(req, res, next) {
@@ -42,7 +41,6 @@ router.get('/', function(req, res, next) {
 //sending Email
 router.post('/send', function(req, res, next) {
 
-  key = getKey(config[0].SENDGRID_API_KEY, process.env.SENDGRID_API_KEY);
   toField = req.body.email;
 
   const sgMail = require('@sendgrid/mail');
@@ -50,8 +48,9 @@ router.post('/send', function(req, res, next) {
   const msg = {
     to: toField,
     from: 'us.business.intel@gmail.com',
-    subject: 'Hi',
-    text: 'Feel free to stop by jacobkocina.com anytime'
+    subject: 'Thanks for your message ' + req.body.name,
+    text: 'This site is a site meant to demonstrate skills learned in a node JS environment. If you would like to read my blog, with links to other portfolio projects, please visit https://www.jacobkocina.com',
+    html: '<h4>Like Tech?</h4><p>This site is a demonstartion of an application created with:</p><ul><li>node.js</li><li>exress.js</li><li>git</li><li>gulp</li><li>pug</li><li>bootstrap</li></ul><p>To read my blog and link to more projects goto https://www.jacobkocina.com</p>'
     /*
     templateId: 'd-0f43a939ab074cb08620a32c4a40c6d6',
     dynamic_template_data: {
@@ -67,16 +66,21 @@ router.post('/send', function(req, res, next) {
 
 
 //this will upload a file and return its contents
-function getKey(local, server) {
-  if (local) {
-    return local;
-  } else if (server) {
-    return server;
+function getKey() {
+  if (!process.env.SENDGRID_API_KEY) {
+    //reading the sendGrid json file if we are in production
+    fs.readFile('data/sendgrid.json', 'utf8', function(err, data){
+        if (err) {
+          throw err;
+        } else {
+          config = JSON.parse(data);
+          return config[0].SENDGRID_API_KEY;
+        }
+    })
+
   } else {
-    console.log("key not found");
+    return process.env.SENDGRID_API_KEY;
   }
-
-
 }
 
 module.exports = router;
